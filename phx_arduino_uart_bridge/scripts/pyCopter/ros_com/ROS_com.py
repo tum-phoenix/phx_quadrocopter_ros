eeae11301302803fab9b9eccadd2ed7cc032a326
Julian Tatsch <tatsch@gmail.com>
2015-01-27 22:23:24 +0100

    implemented batterymsg, euler to quaternion conversion

--- phx_arduino_uart_bridge/scripts/pyCopter/ros_com/ROS_com.py
+++ phx_arduino_uart_bridge/scripts/pyCopter/ros_com/ROS_com.py
@@ -1,20 +1,20 @@
 __author__ = 'manuelviermetz'
-# based on https://github.com/tum-phoenix/phx_controller/blob/master/src/controller_node.py
 
 import time
 import numpy as np
 try:
     import rospy
-    from sensor_msgs.msg import Imu
+    import tf
+    from copter_status import copter
+    from sensor_msgs.msg import Imu, Quaternion
     from sensor_msgs.msg import NavSatFix
     from sensor_msgs.msg import Joy
     from sensor_msgs.msg import FluidPressure #Barometer
     from sensor_msgs.msg import Temperature #For compensation gyrodrift
     from sensor_msgs.msg import Range #Distance to ground
-    from diagnostic_msgs.msg import DiagnosticArray #For Battery status
     from geometry_msgs.msg import Twist
     from phx_arduino_uart_bridge.msg import Motor
-	
+    from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue #For Battery status
 except:
     print 'import rospy failed, have fun...'
 
@@ -26,8 +26,10 @@ class ros_communication():
             imu
             gps
             magnetometer
+            rc
 
             this receives:
+            motor messages
 
         """
         try:
@@ -38,6 +40,8 @@ class ros_communication():
             self.motor_msg = Motor()
             self.ros_publish_gps = rospy.Publisher('/phoenix/stat_gps', NavSatFix, queue_size=10)
             self.NavSatFix_msg = NavSatFix()
+            self.ros_publish_battery = rospy.Publisher('/phoenix/stat_battery', DiagnosticArray, queue_size=10)
+            self.diagnostic_msg = DiagnosticArray()
             self.ros_publish_rc0 = rospy.Publisher('/phoenix/rc_0', Joy, queue_size=10)
             self.Joy_0_msg = Joy()
             self.ros_publish_rc1 = rospy.Publisher('/phoenix/rc_1', Joy, queue_size=10)
@@ -51,6 +55,7 @@ class ros_communication():
         if not copter:
             print 'you have to define an copter to subscribe a ros topic'
         else:
+            self.copter = copter
             # subscribe to the different topics of interest: simple_directions, commands
             self.ros_subscribe_cmd_vel = rospy.Subscriber('/phoenix/cmd_vel', Twist, self.callback_cmd_vel)
             self.ros_subscribe_cmd_motor = rospy.Subscriber('/phoenix/cmd_motor', Motor, self.callback_cmd_motor)
@@ -58,11 +63,12 @@ class ros_communication():
     def listen(self):
         self.rate.sleep()
 
-    def callback_cmd_motor(self, stuff):
-        print ' >>> ROS_callback: received cmd_motor', stuff
+    def callback_cmd_motor(self, motor_msg):
+        print ' >>> ROS_callback: received cmd_motor', motor_msg
+        self.copter
 
-    def callback_cmd_vel(self, stuff):
-        print ' >>> ROS_callback: received cmd_vel', stuff
+    def callback_cmd_vel(self, cmd_vel_msg):
+        print ' >>> ROS_callback: received cmd_vel', cmd_vel_msg
 
     def pub_imu(self, acc=(1, 2, 3), gyr=(4, 5, 6), mag=(7, 8, 9), attitude=(10, 11, 12, 13), debug=False):
         """
@@ -74,6 +80,9 @@ class ros_communication():
             if debug: print 'imu did angular_velocity'
             self.imu_msg.linear_acceleration = acc
             if debug: print 'imu did linear_acceleration'
+            q = tf.transformations.quaternion_from_euler(attitude[0], attitude[1], attitude[2]) 
+            self.imu_msg.orientation = Quaternion(*q)
+            if debug: print 'imu did orientation'
             self.ros_publish_imu.publish(self.imu_msg)
             if debug: print ' >>> sent imu'
         except:
@@ -146,15 +155,12 @@ class ros_communication():
         """
          battery = [ cell1, cell2, cell3, cell4 ]
         """
-        print ' >>> send_battery not implemented'
-
-    def receive_simple_directions(self, directions=(0, 0, 0, 0), debug=False):
-        """
-            this is a very basic approach to set flight directions!
-            simple_directions = [0, 0, 0, 0]   # backward-forward, left-right, up-down, left-right-turn
-        """
-        foo = directions
-        print ' >>> receive_simple_directions not implemented'
+        self.diagnostic_msg.status = DiagnosticStatus(name="Battery",level=DiagnosticStatus.OK,message="OK")
+        self.diagnostic_msg.status.values = [ KeyValue("Cell 1",str(battery[0])),
+                                              KeyValue("Cell 2",str(battery[1])),
+                                              KeyValue("Cell 3",str(battery[2])),
+                                              KeyValue("Cell 4",str(battery[3]))]
+        self.ros_publish_battery.publish(self.diagnostic_msg)
 
 
 if __name__ == '__main__':
@@ -169,72 +175,3 @@ if __name__ == '__main__':
 
     except rospy.ROSInterruptException:
         pass
-
-
-
-"""     ORIGINAL CODE
-class ControllerNode():
-    def __init__(self):
-        self.sub = rospy.Subscriber('/phoenix/imu', Imu, self.imuCallback)
-        self.pub = rospy.Publisher('/phoenix/cmd_motor', MotorMessage)
-        r = rospy.Rate(self.freq)
-
-        self.drone = Drone()
-        self.controller = Controller(self.drone)
-
-        self.xdot_desired = np.zeros((3, 1))
-        self.thetadot_desired = np.zeros((3, 1))
-
-        self.last = rospy.get_time()
-
-        while not rospy.is_shutdown():
-            r.sleep()
-
-    def imuCallback(self, imu_msg):
-        now = rospy.get_time()
-        dt = now - self.last
-        self.last = now
-        #update state
-        self.drone.thetadot[0] = imu_msg.angular_velocity.x
-        self.drone.thetadot[1] = imu_msg.angular_velocity.y
-        self.drone.thetadot[2] = imu_msg.angular_velocity.z
-
-        self.drone.xdoubledot[0] = imu_msg.linear_acceleration.x
-        self.drone.xdoubledot[1] = imu_msg.linear_acceleration.y
-        self.drone.xdoubledot[2] = imu_msg.linear_acceleration.z
-
-        self.drone.xdot = self.drone.xdot#+self.drone.xdoubledot*dt
-
-        #calculate motor commands
-        cmds = self.controller.calculate_control_command(self.xdot_desired, self.thetadot_desired.item(2))
-        print "cmds",cmds
-        cmds = np.clip(cmds, 0, 3)
-        print "clipped cmds",cmds
-
-        cmds = self.zero_rpm +cmds*(self.full_rpm-self.hover_rpm)
-        cmds = np.clip(cmds, 2500, 2800)
-
-
-        #publish to topic/uart bridge
-        motor_msg = MotorMessage()
-        motor_msg.motor0 = cmds[0] #VL
-        motor_msg.motor1 = cmds[1] #VR
-        motor_msg.motor2 = cmds[2] #HL
-        motor_msg.motor3 = cmds[3] #HR
-        self.pub.publish(motor_msg)
-
-        #test
-        #for rpm in range(2400, 2900, +1):
-        #   motor_msg.motor0 = rpm
-        #   motor_msg.motor1 = rpm
-        #   motor_msg.motor2 = rpm
-        #   motor_msg.motor3 = rpm
-        #   self.pub.publish(motor_msg)
-
-if __name__ == '__main__':
-    rospy.init_node('controller')
-    try:
-        controller_node = ControllerNode()
-    except rospy.ROSInterruptException: pass
-
-"""
