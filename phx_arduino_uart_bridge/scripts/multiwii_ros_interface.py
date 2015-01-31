3c5b75a354494f95c023c837b1a69077c3e07c1a
Manuel <manuel.viermetz@tum.de>
2015-01-31 16:12:44 +0100

    added multiwii_ros_interface.py which should be a fast bridge between ros and multiwii. so there is no connection to marvic established.
    OSC is deactivated for now.

---
+++ phx_arduino_uart_bridge/scripts/multiwii_ros_interface.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+
+import pyCopter
+print '> import done'
+
+# we first start the copter by connecting to intermediate_arduino and multiwii.
+# At this point also the OSC network connections are established.
+# later also the ros_com should be part of pyCopter.
+phoenix = pyCopter.copter(con_multiwii=True, con_intermediate=False, con_ros=True, osc_transmit=False, osc_receive=False)
+
+# by starting the copter we can now access two 'read-only' remote_controls
+multiwii_radio = phoenix.serial_multiwii        # this is a 'read-only' RC.
+
+# finally we tell the copter which RC should be used by default
+phoenix.use_rc = False
+
+# speed test
+phoenix_update_speed_test = pyCopter.speedtest()
+
+try:
+    while True:
+        # fist thing to do in the main loop is to update all variables of the copter, including updates to the serial clients.
+        # This is all done by the pyCopter.update() method. You do not have to worry about timings, just call this line very often.
+        phoenix_update_speed_test.start()
+        phoenix.update()
+        phoenix_update_speed_test.stop()
+        
+        phoenix_update_speed_test.print_result(rate=1., text=' > phoenix update takes')
+
+except KeyboardInterrupt:
+    phoenix.stop()
+print 'phoenix_os_1.py stopped'
