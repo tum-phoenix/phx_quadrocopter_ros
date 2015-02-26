2cb06b774b33acbf91e48d30af956bf3eec1360d
TUM Phoenix <phoenix@phoenix.(none)>
2015-02-26 23:11:48 +0100

    bugfix battery

--- phx_arduino_uart_bridge/scripts/marvic_test.py
+++ phx_arduino_uart_bridge/scripts/marvic_test.py
@@ -7,10 +7,11 @@ mw = pyCopter.serial_com.multiwii_protocol('/dev/marvic')
 mw.startup_delay = 10.0
 
 while True:
-    time.sleep(0.25)
+    time.sleep(0.02)
     #mw.get_msg(cmd_list=[101, 102, 104, 105, 106, 108, 109], debug=True)
-    mw.get_msg(cmd_list=[101], debug=False)
-    mw.receive(debug=False)
+    mw.get_msg(cmd_list=[66], debug=True)
+    mw.receive(debug=True)
     #print mw.attitude
     print mw.status
+    print mw.battery
     #print mw.raw_imu
