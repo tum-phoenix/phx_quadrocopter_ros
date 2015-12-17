6a0eccfec18499192cc15d132b0f3bfc53b2b2ec
phoenix <flight@tum-phoenix.de>
2015-12-18 07:24:05 +1030

    added footprint transform

--- phx_uart_msp_bridge/scripts/transform_lidar_altitude.py
+++ phx_uart_msp_bridge/scripts/transform_lidar_altitude.py
@@ -2,11 +2,37 @@
 
 import numpy as np
 import rospy
+
+import tf
+
+import tf2_ros
+import geometry_msgs.msg
+
 from phx_uart_msp_bridge.msg import Altitude
 from phx_uart_msp_bridge.msg import Attitude
 
 pitch_angle = 0    # rad
-roll_angle = 0     # rad
+roll_angle = 0     # rad#
+
+
+def update_footprint_transform(height):
+    br = tf2_ros.TransformBroadcaster()
+    t = geometry_msgs.msg.TransformStamped()
+
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = "base_footprint"
+    t.child_frame_id = "base_stabelized"
+    t.transform.translation.x = 0.0
+    t.transform.translation.y = 0.0
+    t.transform.translation.z = height
+    q = tf.transformations.quaternion_from_euler(0, 0, 0)
+    t.transform.rotation.x = 0
+    t.transform.rotation.y = 0
+    t.transform.rotation.z = 0
+    t.transform.rotation.w = 1
+
+    br.sendTransform(t)
+
 
 def convert_altitude_measurement(input_altitude):
     global pitch_angle, roll_angle
@@ -14,6 +40,8 @@ def convert_altitude_measurement(input_altitude):
     new_msg = Altitude()
     new_msg.estimated_altitude = new_altitude
     ros_publish_new_altitude.publish(new_msg)
+    update_footprint_transform(new_altitude)
+
 
 def receive_attitude(input_attitude):
     global pitch_angle, roll_angle
