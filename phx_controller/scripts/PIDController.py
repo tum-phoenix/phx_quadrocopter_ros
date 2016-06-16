4f7661b517b644a027f4223fc1cbff3ac40fe24f
Valep42 <p.czempin@gmx.de>
2016-06-16 19:02:49 +0200

    added choice between i-modes to PID controller

--- phx_controller/scripts/PIDController.py
+++ phx_controller/scripts/PIDController.py
@@ -1,9 +1,10 @@
 import numpy as np
 class PIDController():
-    def __init__(self, controlCommand, setPoint_p,p,d,i,setPoint_d, i_stop):
+    def __init__(self, controlCommand, setPoint_p,p,d,i,setPoint_d, i_stop, i_mode):
         self.set_point = setPoint_p
-
+        self.i_mode = i_mode # with i mode to use, calculation of i value differs in different controller
         self.controlCommand = controlCommand
+        self.previousAltitude = 0
 
         self.p = p
         self.d = d
@@ -14,7 +15,13 @@ class PIDController():
 
     def calculateControlCommand(self, current_p, current_d):
 
-        self.sum_i += self.set_point - current_p
+        if(self.i_mode == 0):
+            self.sum_i += self.set_point - current_p
+        elif self.i_mode == 1: # used by take off controller
+            if (self.previousAltitude >= current_p):
+                self.i_sum += 1
+            else:
+                self.i_sum = 0
 
         if self.sum_i >= self.i_stop:
             self.sum_i = self.i_stop
@@ -25,6 +32,8 @@ class PIDController():
         controlCommand_i = self.sum_i * self.i
         unclipped =  self.controlCommand + controlCommand_p + controlCommand_d + controlCommand_i
         self.controlCommand = np.clip(unclipped,1000,2000)
+
+        self.previousAltitude = current_p # used by take off controller
         return self.controlCommand
 
 
