10a8e9d4102eec9d5d7607b87670ad7f87b97bc6
Luca <luca.evangelisti@tum.de>
2017-06-09 13:16:37 +0200

    added linear interpolation between initial and goal pose number of points to be interpolated between initial and goal is specified in launchfile with parameter "num_points"

--- phx_trajectory_planner/src/path_planner.cpp
+++ phx_trajectory_planner/src/path_planner.cpp
@@ -19,6 +19,25 @@ void goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
   goal.pose = msg->pose;
 }
 
+// returns array of float64s of difference vector between Poses initial and goal: [x,y,z] (not Quaternions)
+// initial and goal are poses
+// dVector must be an array of doubles with 3 elements
+// divides dVector's length into n equal pieces
+void linear_pose_diff(geometry_msgs::Pose initial, geometry_msgs::Pose goal, double* dVector, int n)
+{
+    dVector[0] = (goal.position.x - initial.position.x)/n;
+    dVector[1] = (goal.position.y - initial.position.y)/n;
+    dVector[2] = (goal.position.z - initial.position.z)/n;
+}
+
+// adds x,y,z components of double[x,y,z] step_vector array to intermediate
+void add_step(geometry_msgs::Pose* intermediate, double* step_vector)
+{
+    intermediate->position.x += step_vector[0];
+    intermediate->position.y += step_vector[1];
+    intermediate->position.z += step_vector[2];
+}
+
 int main(int argc, char** argv)
 {
 
@@ -29,20 +48,39 @@ int main(int argc, char** argv)
   ros::Subscriber init_sub = nh.subscribe("/phx/pose", 1000, initialCallback);
   ros::Subscriber goal_sub = nh.subscribe("/phx/current_goal", 1000, goalCallback);
 
-
   ros::Publisher pathpub = nh.advertise<nav_msgs::Path>("/phx/path", 1000);
 
-  // do shit
+  // number of points to interpolate between goal and initial
+  int num_pts = 0;
+  nh.getParam("/path_planner/num_points", num_pts);
 
-  ros::Rate loop_rate(10);
+  // CHECKEN dass parameter ankommt
+  //std::cout << "num_points: " << num_pts << std::endl;
 
-
+  ros::Rate loop_rate(1);
 
   while (ros::ok()){
     nav_msgs::Path path;
     std::vector<geometry_msgs::PoseStamped> posestoadd;
 
     posestoadd.push_back(initial);
+
+    if(num_pts > 0)
+    {
+        // richtungsvektor ausrechnen
+        double dVector[3] = {0};
+        linear_pose_diff(initial.pose, goal.pose, dVector, num_pts);
+
+        geometry_msgs::PoseStamped intermediate = initial;
+
+        for(int i = 0; i < num_pts; i++)
+        {
+            // von initial zu goal gradient addieren
+            add_step(&(intermediate.pose), dVector);
+            posestoadd.push_back(intermediate);
+        }
+    }
+
     posestoadd.push_back(goal);
 
     path.poses = posestoadd;
