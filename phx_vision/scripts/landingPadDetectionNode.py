1b88ccb49cc3e11f3191a537a7f4bcdc6352847f
Julian Tatsch <julian@tatsch.it>
2017-06-24 22:15:05 +0200

    primary round of refactoring in phx_vision

--- phx_vision/scripts/landingPadDetectionNode.py
+++ phx_vision/scripts/landingPadDetectionNode.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import roslib
-roslib.load_manifest('phx_vision')
 import sys
 import rospy
 import cv2
@@ -8,47 +7,49 @@ import numpy as np
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+roslib.load_manifest('phx_vision')
 
-class image_converter:
 
-  def __init__(self):
-    cv2.namedWindow("Image window", 1)
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("image_mono", Image, self.callback)
+class image_converter(object):
+    def __init__(self):
+        cv2.namedWindow("Image window", 1)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("image_mono", Image, self.callback)
 
-  def callback(self, data):
-    try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
-      resizedGreyImage = cv_image
-    except CvBridgeError, e:
-      print e
+    def callback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
+            resizedGreyImage = cv_image
+        except CvBridgeError, e:
+            print(e)
 
-    #REad from file for debug purposes
-    #image = cv2.imread("pad.png")
-    #greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Read from file for debug purposes
+    # image = cv2.imread("pad.png")
+    # greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    #resizedGreyImage = cv2.resize(greyImage, (0,0), fx=0.125, fy=0.125)
-    (T, thresholdedImage) = cv2.threshold(resizedGreyImage, 200, 255, cv2.THRESH_TOZERO)
+    # resizedGreyImage = cv2.resize(greyImage, (0,0), fx=0.125, fy=0.125)
+    (_, thresholdedImage) = cv2.threshold(resizedGreyImage, 200, 255,
+                                          cv2.THRESH_TOZERO)
 
-    sigma = 0.33
-    v = np.median(resizedGreyImage)
     lower = 245
     upper = 260
 
     edgedImage = cv2.Canny(resizedGreyImage, lower, upper)
     cv2.imshow("Image", resizedGreyImage)
     cv2.imshow("edgedImage", edgedImage)
-    #cv2.imshow("ThresholdedImage", thresholdedImage)
+    # cv2.imshow("ThresholdedImage", thresholdedImage)
     cv2.waitKey(1)
 
+
 def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print "Shutting down"
-  cv2.destroyAllWindows()
+    image_converter()
+    rospy.init_node('image_converter', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+        cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main(sys.argv)
--- phx_vision/scripts/landing_detection_2206.py
+++ phx_vision/scripts/landing_detection_2206.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-q
-'''
+"""
 Created on June 22, 2016
 
 @author: hidenobu_matsuki 松木秀伸
-'''
+"""
 
 # import the necessary packages
 import argparse
@@ -29,14 +29,13 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (7, 7), 0)
-    ret,th3 = cv2.threshold(blurred,80,255,cv2.THRESH_BINARY_INV)
+    ret, th3 = cv2.threshold(blurred, 80, 255, cv2.THRESH_BINARY_INV)
     kernel = np.ones((10, 10), np.uint8)
     dilation = cv2.dilate(th3, kernel, iterations=1)
 
     # find contours in the edge map
     cnts = cv2.findContours(dilation.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]
-
+                            cv2.CHAIN_APPROX_SIMPLE)[-2]
 
     # loop over the contours
     for c in cnts:
@@ -45,7 +44,7 @@ while True:
         approx = cv2.approxPolyDP(c, 0.01 * peri, True)
 
         # ensure that the approximated contour is "roughly" rectangular
-        if len(approx) >=10 and len(approx) <= 20: #18はあった方が良い
+        if len(approx) >= 10 and len(approx) <= 20:
             # compute the bounding box of the approximated contour and
             # use the bounding box to compute the aspect ratio
             (x, y, w, h) = cv2.boundingRect(approx)
@@ -53,19 +52,19 @@ while True:
             # compute the solidity of the original contour
             area = cv2.contourArea(c)
 
-            #compute the minimum Rectangle, which is constant to rotation.
+            # compute the minimum Rectangle, which is constant to rotation.
             rect = cv2.minAreaRect(c)
             box = cv2.boxPoints(rect)
 
-            #compute aspect ratio
-            l1 = math.sqrt((box[1,0]-box[0,0])**2+(box[1,1]-box[0,1])**2)
-            l2 = math.sqrt((box[2,0]-box[1,0])**2+(box[2,1]-box[1,1])**2)
+            # compute aspect ratio
+            l1 = math.sqrt((box[1, 0]-box[0, 0])**2+(box[1, 1]-box[0, 1])**2)
+            l2 = math.sqrt((box[2, 0]-box[1, 0])**2+(box[2, 1]-box[1, 1])**2)
             box = np.int0(box)
-            width = max(l1,l2)
-            height = min(l1,l2)
+            width = max(l1, l2)
+            height = min(l1, l2)
             aspectRatio = width / height
 
-            #compute solidity
+            # compute solidity
             hullArea = cv2.contourArea(box)
             solidity = area / float(hullArea)
 
@@ -73,8 +72,7 @@ while True:
             # aspect ratio of the contour falls within appropriate bounds
             keepSolidity = solidity > 0.5 and solidity < 0.9
             keepAspectRatio = (aspectRatio >= 1.7 and aspectRatio <= 2.0) or (aspectRatio >= 1 and aspectRatio <= 1.1)
-            keeparea = area >1300 and area < 120000
-
+            keeparea = area > 1300 and area < 120000
 
             # ensure that the contour passes all our tests
             if keeparea and keepSolidity and keepAspectRatio:
@@ -92,16 +90,13 @@ while True:
                 cv2.line(frame, (startX, cY), (endX, cY), (0, 0, 255), 3)
                 cv2.line(frame, (cX, startY), (cX, endY), (0, 0, 255), 3)
 
-
     # draw the status text on the frame
-    cv2.putText(frame, status, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-        (0, 0, 255), 2)
+    cv2.putText(frame, status, (20, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     # show the frame and record if a key is pressed
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
-
-
 
     # if the 'q' key is pressed, stop the loop
     if key == ord("q"):
