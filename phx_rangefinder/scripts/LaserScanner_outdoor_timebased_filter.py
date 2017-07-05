d629f39d19a32526500c851431b8993f9f886f16
Marian Kannwischer <marian.kannwischer@t-online.de>
2017-07-05 19:29:14 +0200

    bugfixes and comments

--- phx_rangefinder/scripts/LaserScanner_outdoor_timebased_filter.py
+++ phx_rangefinder/scripts/LaserScanner_outdoor_timebased_filter.py
@@ -10,7 +10,7 @@ COUNT = 0
 # Size of array. Increase for better median filtering
 SIZE = 5
 
-# Array of range arrays
+# Array of range data arrays
 data_2d = np.array([])
 
 def callback_LaserScan_timebased_filter(new_LaserScan=LaserScan()):
@@ -21,27 +21,25 @@ def callback_LaserScan_timebased_filter(new_LaserScan=LaserScan()):
     # Set invalid values to zero
     data[np.isnan(data)] = 0
 
-    if not data_2d:
-        data_2d = numpy.array([data]*SIZE)
-    
-    print ("Temp shape", np.shape(temp))
-    if COUNT => SIZE: COUNT = 0
+    # Initialize data array
+    if len(data_2d) == 0:
+        data_2d = np.array([data]*SIZE)
+
+    if COUNT >= SIZE: COUNT = 0
+
+    # Update column with new scan and filter
     data_2d[COUNT] = data
     COUNT += 1
     result = np.median(data_2d, axis=0)
-    
-    print (result, len(result))
-    
+
     # Publish filtered ranges
     new_LaserScan.ranges = result
     pub_scan_filtered.publish(new_LaserScan)
 
 
-
-
 rospy.init_node('LaserScanner_outdoor_timebased_filter')
 ros_subscribe_LaserScan = rospy.Subscriber('/scan_filtered', LaserScan, callback_LaserScan_timebased_filter)
-pub_cost_range = rospy.Publisher('/scan_filtered_timebased_median', LaserScan, queue_size=1)
+pub_scan_filtered = rospy.Publisher('/scan_filtered_timebased_median', LaserScan, queue_size=1)
 
 r = rospy.Rate(1)
 while not rospy.is_shutdown():
