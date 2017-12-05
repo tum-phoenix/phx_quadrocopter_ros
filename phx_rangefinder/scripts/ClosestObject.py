9085718f1683141e14bbef3b63e107a0b3f693a7
Quexor <marian.kannwischer@t-online.de>
2017-12-05 19:56:34 +0100

    Added closest object detection and broadcasting a vector of the closest object.

--- phx_rangefinder/scripts/ClosestObject.py
+++ phx_rangefinder/scripts/ClosestObject.py
@@ -1,35 +1,48 @@
 import numpy as np
 import math
 import rospy
+from phx_rangefinder.msg import ClosestObjectVector
 from sensor_msgs.msg import LaserScan
 
 
 # Input: Number of the scan value, start angle value (rad), angle increment (rad)
 # Output: angle of scan value (rad)
 def get_Angle(index, angle_min, angle_inc):
-    return angle_min + index * angle_inc
+    return (angle_min + index * angle_inc)
 
 
 def callback_ClosestObject(new_LaserScan=LaserScan()):
     data = np.array(new_LaserScan.ranges)
+
     angle_min = new_LaserScan.angle_min
     angle_inc = new_LaserScan.angle_increment
 
     # Masking nan values
-    masked_data = np.ma.masked_invalid(data)
+    masked_data = np.ma.masked_invalid(data).compressed()
     # Indices of min values
-    min_indices = np.ma.where(masked_data == masked_data.min())
+    min_indices = np.array(masked_data == masked_data.min()).nonzero()[0]
+    #print(min_indices)
 
-    if len(min_indices) >= 1:
+    if len(min_indices) >= 2:
         # Mutiple closest object (distances are the same)
-        vector_sum = np.array(0, 0)
+        vector_sum = np.zeros(2)
+        #print(min_indices)
         for index in min_indices:
-            distance = data[index]
+            distance = masked_data[index]
+
+            #print(index)
             angle = get_Angle(index, angle_min, angle_inc)
-            result = np.array(distance * math.cos(angle), distance * math.sin(angle))
+            #print("angle: {}, distance: {}".format(angle, distance))
+            result = np.array([distance * math.cos(angle), distance * math.sin(angle)])
+            #print(result)
+            #print(result.shape)
+            #print(vector_sum)
+            #print(vector_sum.shape)
             vector_sum += result
 
-        radius = np.sqrt(vector_sum[0]**2 + vector_sum[0]**2)
+        radius = np.sqrt(vector_sum[0]**2 + vector_sum[1]**2)
+        #print(vector_sum)
+        #print(radius)
         if radius == 0:
             pub_closest_object.publish(0, 0)
         else:
@@ -38,19 +51,13 @@ def callback_ClosestObject(new_LaserScan=LaserScan()):
     else:
         # Only one closest object
         angle = get_Angle(min_indices[0], angle_min, angle_inc)
-        pub_closest_object.publish(angle, data[angle_min[0]])
-
-    for x in np.nditer(min_indices):
-        # Calculate angles
-        angle = math.degrees(angle_min + x * angle_inc)
-        print("Min value", data[x], "Angle", angle)
-        pub_closest_object.publish(angle, data[x], )
+        pub_closest_object.publish(angle, masked_data[min_indices[0]])
 
 
 rospy.init_node('Closest_Object_Node')
 ros_subscribe_LaserScan = rospy.Subscriber(
     '/scan_filtered', LaserScan, callback_ClosestObject)
-pub_closest_object = rospy.Publisher("closest_object", LaserScan, queue_size=1)
+pub_closest_object = rospy.Publisher("closest_object", ClosestObjectVector, queue_size=1)
 
 r = rospy.Rate(1)
 while not rospy.is_shutdown():
