2e79f0e3074cd078fafb433251a3297abb0e7831
Luca Evangelisti <luca.evangelisti@tum.de>
2018-09-16 23:05:47 +0200

    added matlab files for plotting data live in simulink

---
+++ phx_trajectory_planner/rosnetwork.m
@@ -0,0 +1,15 @@
+ % vorher in /etc/hosts ip adresse von phoenix-nuc auf eigenem pc ändern
+ 
+ %phoenix wifi
+%  setenv('ROS_MASTER_URI','http://192.168.0.53:11311');
+%  setenv('ROS_IP','192.168.0.104');
+%  rosinit('http://192.168.0.53:11311', 'NodeHost','192.168.1.1', 'NodeName', 'Test') % replace own IP Adress
+ 
+ %copter hotspot
+ setenv('ROS_MASTER_URI','http://10.42.0.1:11311');
+ setenv('ROS_IP','10.42.0.141');
+ rosinit('http://10.42.0.1:11311', 'NodeHost','192.168.1.1', 'NodeName', 'Test') % replace own IP Adress
+ 
+ %Sub = rossubscriber('/chatter')
+%  Sub = rossubscriber('/phx/fc/attitude')
+%  scandata = receive(Sub,3)
