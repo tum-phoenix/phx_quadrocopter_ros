cb4b8bbbb49349bd074c9b5259cb405108837e57
Kapsl0ck <arne.kaps@tum.de>
2018-11-12 18:41:29 +0100

    Improved msg generation in Simulink

--- phx_trajectory_planner/rosnetwork.m
+++ phx_trajectory_planner/rosnetwork.m
@@ -4,11 +4,15 @@
 %  setenv('ROS_MASTER_URI','http://192.168.0.53:11311');
 %  setenv('ROS_IP','192.168.0.104');
 %  rosinit('http://192.168.0.53:11311', 'NodeHost','192.168.1.1', 'NodeName', 'Test') % replace own IP Adress
+ setenv('ROS_MASTER_URI','http://10.152.196.70:11311');
+ setenv('ROS_IP','192.168.0.103');
+ rosinit('http://10.152.196.70:11311', 'NodeHost','192.168.1.1', 'NodeName', 'Test') % replace own IP Adress
+
  
  %copter hotspot
- setenv('ROS_MASTER_URI','http://10.42.0.1:11311');
- setenv('ROS_IP','10.42.0.141');
- rosinit('http://10.42.0.1:11311', 'NodeHost','192.168.1.1', 'NodeName', 'Test') % replace own IP Adress
+%  setenv('ROS_MASTER_URI','http://10.42.0.1:11311');
+%  setenv('ROS_IP','10.42.0.141');
+%  rosinit('http://10.42.0.1:11311', 'NodeHost','192.168.1.1', 'NodeName', 'Test') % replace own IP Adress
 
 %local test
 %  setenv('ROS_MASTER_URI','http://192.168.56.1:11311');
